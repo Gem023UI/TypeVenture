93de9d3a0cfbed7d874f372e1a950e6aad5b22dd
Jemuel Malaga <malagajemuel@gmail.com>
2025-09-29 21:15:58 +0800

    updated info-1

--- frontend/src/components/LandingSection.jsx
+++ frontend/src/components/LandingSection.jsx
@@ -3,18 +3,23 @@ import "./LandingSection.css";
 
 export default function LandingSection({ logoUrl }) {
   return (
-    <section className="landing">
-      <div>
+    <section className="landing-page">
+      <div className="landing-info-1">
         <div className="landing-logo-container">
-        <img src={logoUrl} alt="Typeventure Logo" className="landing-logo" />
+          <img src={logoUrl} alt="Typeventure Logo" className="landing-logo" />
         </div>
-        <h1 className="title">TYPE<span className="venture">VENTURE</span></h1>
-        <div className="btn-container">
-          <button className="btn primary">LOGIN / REGISTER</button>
-          <button className="btn secondary">PLAY AS GUEST</button>
+        <div className="name-buttons">
+          <div className="name-container">
+            <h1 className="type">TYPE<span className="venture">VENTURE</span></h1>
+            <p className="description">Discover typography rules through fun, interactive challenges that turn learning into play.</p>
+          </div>
+          <div className="btn-container">
+            <button className="login-btn">LOGIN / REGISTER</button>
+            <button className="guest-btn">PLAY AS GUEST</button>
+          </div>
         </div>
       </div>
-      <div>
+      <div className="landing-info-2">
         <h2>
           Play with <span className="highlight-pink">TYPE</span>
           <br />
