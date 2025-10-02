de5588b3a7f73e99105cf269f81015e9fc394589
Jemuel Malaga <malagajemuel@gmail.com>
2025-10-02 20:07:37 +0800

    fix deployment 4

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const registerUser = async (formData) => {
   try {
