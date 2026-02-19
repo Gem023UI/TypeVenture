18bef1fc0ee277eff095e2d8ea68d62178a341bb
Gem023UI <malagajemuel@gmail.com>
2026-02-20 04:39:45 +0800

    initiate deployment

--- frontend/src/components/pages/Login&Register.jsx
+++ frontend/src/components/pages/Login&Register.jsx
@@ -9,8 +9,8 @@ import Loader from "../layout/Loader";
 import "./Login&Register.css";
 
 // API base URL
-// const API_URL = import.meta.env.VITE_BACKEND_URL || "https://typeventure.onrender.com";
-const API_URL = import.meta.env.VITE_LOCAL_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_BACKEND_URL || "https://typeventure.onrender.com";
+// const API_URL = import.meta.env.VITE_LOCAL_URL || "http://localhost:5000";
 
 export default function LoginRegister({ logoUrl }) {
   const [isActive, setIsActive] = useState(false);
