a557dd7e0169af40d56703e487f4eead7af001fb
Gem023UI <malagajemuel@gmail.com>
2026-02-25 12:21:46 +0800

    railway deployment: 2

--- frontend/src/api/emailVerify.js
+++ frontend/src/api/emailVerify.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = import.meta.env.VITE_BACKEND_URL || "https://typeventure.onrender.com";
-const API_URL = import.meta.env.VITE_LOCAL_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_BACKEND_URL || "https://typeventure.onrender.com";
+// const API_URL = import.meta.env.VITE_LOCAL_URL || "http://localhost:5000";
 
 const BASE_URL = `${API_URL}/api/user`;
 
